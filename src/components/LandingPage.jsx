e82298b429308e89530f78cb74b3e8425fac9402
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2020-12-30 19:43:06 +0500

    Updation

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      
+      <div style={{
+        height: '30px', 
+        width: '10px'
+      }}>
+      <img src="/image/image1.jpg" class="img-fluid" alt="Responsive image"/>
+      </div>
+    </div>
+    
+  );
+};
+
+
+export default LandingPage;
