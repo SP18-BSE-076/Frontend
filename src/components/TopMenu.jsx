cd154665a1718eb7a2b1653f9047e109792562f1
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-08 00:39:54 +0500

    Update TopMenu.jsx

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,37 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-// import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button, FormControl, Toolbar, Typography } from '@material-ui/core';
-import userService from '../services/UserService';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Nav, Navbar} from 'react-bootstrap';
- const useStyles = makeStyles((theme) => ({
-    Name: {
-       color:"black",
-     },
-   }));
-const TopMenu = () => {
-    const classes = useStyles();
-     return ( 
-    
-    ////////////
-    <Navbar bg="dark" variant="light">
-    <Nav className="mx-auto">
-      <Nav.Link href="/" style={{color:"white"}}>Home</Nav.Link>
-      <Nav.Link href="/products" style={{color:"white"}}>Typography</Nav.Link>
-      <Nav.Link href="/contact-us" style={{color:"white"}}>Contact Us</Nav.Link>
-      {!userService.isLoggedin()? <>
-        <Nav.Link href="/login" style={{color:"white"}}>Login</Nav.Link>
-        <Nav.Link href="/register" style={{color:"white"}}>Register</Nav.Link>   
-             </>:<Button variant="contained"  onClick={(e)=>{
-                 userService.logout();
-                 window.location.reload();
-             }}>Log Out {userService.getLoggedinUser().name}</Button>}
-    </Nav>
-  </Navbar>
-    );
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
 
-}
- 
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import userService from "../services/UserService";
+const useStyles = makeStyles((theme) => ({
+  link: {
+    color: "orange",
+    paddingRight: "1rem",
+    
+  },
+}));
+
+const TopMenu = () => {
+  const classes = useStyles();
+  return (
+    <div style={{
+      border: '2px solid orange',
+      
+    }}>
+    <AppBar position="relative" >
+      <Toolbar>
+        <Typography variant="h6">
+          <Link to="/" className={classes.link}>
+            Home
+          </Link>
+        </Typography>
+        <Typography variant="h6">
+          <Link to="/products" className={classes.link}>
+            Products
+          </Link>
+        </Typography>
+
+        <Typography variant="h6">
+          <Link to="/contact-us" className={classes.link}>
+            Contact Us
+          </Link>
+        </Typography>
+        {!userService.isLoggedIn() ? (
+          <>
+            <Typography variant="h6">
+              <Link to="/login" className={classes.link}>
+                Login
+              </Link>
+            </Typography>
+            <Typography variant="h6">
+              <Link to="/register" className={classes.link}>
+                Register
+              </Link>
+            </Typography>
+          </>
+        ) : (
+          <Button
+            variant="contained"
+            color="secondry"
+            onClick={(e) => {
+              userService.logout();
+              window.location.reload();
+            }}
+          >
+            LogOut {userService.getLoggedInUser().name}
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+    </div>
+  );
+};
+
 export default TopMenu;
