16ecca97707ef1b6e79d3885171738d63fa270d8
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-08 00:44:04 +0500

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,41 +1,42 @@
-import Generic from "./Generic";
-import jwt_decode from "jwt-decode";
-class UserService extends Generic
-{
-    constructor(){ super();}
-
-    login =(email,password)=> new Promise((resolve,reject)=>{
-        this.post("users/login",{email,password}).then(token=>{
-            localStorage.setItem("token",token);
-            resolve(token);
-            }).catch(err=>{
-                reject(err);
+import GenericService from "./GenericService";
+import jwtDecode from "jwt-decode";
+class UserService extends GenericService {
+  constructor() {
+    super();
+  }
+  login = (email, password) =>
+    new Promise((resolve, reject) => {
+      this.post("users/login", { email, password })
+        .then((token) => {
+          localStorage.setItem("token", token);
+          resolve(token);
         })
-    }); 
-    register =(name,email,password)=>this.post("users/register",{password,email,name});
-    logout = ()=> {
-        localStorage.removeItem("token");
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  register = (name, email, password) =>
+    this.post("users/register", { password, email, name });
+  logout = () => {
+    localStorage.removeItem("token");
+  };
+  isLoggedIn = () => {
+    return localStorage.getItem("token") ? true : false;
+  };
+  getLoggedInUser = () => {
+    try {
+      const jwt = localStorage.getItem("token");
+      return jwtDecode(jwt);
+    } catch (ex) {
+      return null;
     }
-    isLoggedin = ()=>{
-        return localStorage.getItem("token")?true : false;
-    }
-
-    getLoggedinUser = ()=>{
-        try{
-            const jwt = localStorage.getItem("token");
-            return jwt_decode(jwt);
-        }catch(ex){
-            return null
-        }
-    };
-
-    isAdmin = ()=>{
-        if(this.isLoggedin())
-        {
-            if(this.getLoggedinUser().role=="admin") return true;
-            else return false;
-        } else return false;
-    }
+  };
+  isAdmin = () => {
+    if (this.isLoggedIn()) {
+      if (this.getLoggedInUser().role == "admin") return true;
+      else return false;
+    } else return false;
+  };
 }
 
 let userService = new UserService();
