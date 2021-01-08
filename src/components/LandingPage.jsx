e28d0009652f021781f0c7a508164d74f9d0ff18
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-09 01:54:12 +0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div>
-      <h1><img src="/image/logo.jpg" alt=""/></h1>
+      <h1 class = "logo"><img src="/image/logo.jpg" alt=""/></h1>
       
       <div style={{
         height: '30px',
