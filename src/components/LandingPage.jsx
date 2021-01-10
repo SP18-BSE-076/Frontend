fcfd611464813864f0a142053ea423e1abcf7748
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-10 20:07:23 +0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,6 +6,8 @@ const LandingPage = () => {
       
       <div>
       <img src="/image/image1.jpg" class="img-fluid" alt="Responsive image" width="100%" height="100%"/>
+      <img src="https://images.unsplash.com/photo-1565004569929-4f400c11e9ef?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjB8fHBob3RvZ3JhcGh5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" width="40%" height="40%"/>
+      <img src="https://images.unsplash.com/photo-1542038784456-1ea8e935640e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGhvdG9ncmFwaHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="img-fluid" alt="Responsive image" width="40%" height="40%"/>
       </div>
     </div>
     
