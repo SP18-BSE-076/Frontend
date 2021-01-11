d9482601ec451461aa3cee43390e60d75cb3f3a8
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-11 22:10:19 +0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 const LandingPage = () => {
   return (
     <div>
@@ -6,8 +7,10 @@ const LandingPage = () => {
       
       <div>
       <video src="image/video1.mp4" autoPlay loop muted width="100%" height="100%"></video>
-      <h1></h1>
-      <button className="btns" buttonStyle= "btn--outline" buttonSize="btn--large">Get Started</button>
+      <video src="image/video2.mp4" autoPlay loop muted width="40%" height="40%"></video>
+      <p>Every photographer draws inspiration from many different sources, and documentaries following a handful of the masters of the art are a fantastic starting point. We’ve sorted through the hundreds and picked our favourite examples of these geniuses at the top of their game. All of these documentaries can be watched online and most of them for free.
+
+If you ever dreamt of being a National Geographic photographer, travelling the world shooting the covers of Vogue or documenting a slice of time on the streets of New York, watch on.</p>
       </div>
     </div>
     
