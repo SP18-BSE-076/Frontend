b71bc1c262b671558c5828d28a213d5785a19956
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-11 22:26:09 +0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,10 +8,8 @@ const LandingPage = () => {
       <div>
       <video src="image/video1.mp4" autoPlay loop muted width="100%" height="100%"></video>
       <video src="image/video2.mp4" autoPlay loop muted width="40%" height="40%"></video>
-      <p>Every photographer draws inspiration from many different sources, and documentaries following a handful of the masters of the art are a fantastic starting point. We’ve sorted through the hundreds and picked our favourite examples of these geniuses at the top of their game. All of these documentaries can be watched online and most of them for free.
-
-If you ever dreamt of being a National Geographic photographer, travelling the world shooting the covers of Vogue or documenting a slice of time on the streets of New York, watch on.</p>
       </div>
+      <p>Every photographer draws inspiration from many different sources, and documentaries following a handful of the masters of the art are a fantastic starting point. We’ve sorted through the hundreds and picked our favourite examples of these geniuses at the top of their game. All of these documentaries can be watched online and most of them for free.</p>
     </div>
     
   );
