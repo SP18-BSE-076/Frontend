0b66bf5333b7cf25702b6d306296fe774ff47e0a
SP18-BSE-076 <71717868+SP18-BSE-076@users.noreply.github.com>
2021-01-13 19:44:52 +0500

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,77 +1,76 @@
-import React from "react";
+  
+import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import { TextField, Button } from "@material-ui/core";
 import userService from "../../services/UserService";
-
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "300px",
-  },
-  child: {
-    width: "60%",
-  },
-}));
-const ContactUs = (props) => {
-  const classes = useStyles();
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  return (
-    <div className={classes.container}>
-      <div className={classes.child}>
-        <TextField
-          label="email"
-          fullWidth
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />{" "}
-        <br />
-        <TextField
-          label="Name"
-          type="Name"
-          fullWidth
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />{" "}
-        <br />
-        <br />
-        <TextField
-          label="Message"
-          type="Meassage"
-          fullWidth
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />{" "}
-        <br />
-        <br />
-        <Button
-          variant="contained"
-          color="dark"
-          onClick={(e) => {
-            userService
-              .login(email, password)
-              .then((data) => {
-                console.log(data);
-                window.location.href = "/";
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          }}
-        >
-          Contact Us
-        </Button>
-      </div>
-    </div>
-  );
-};
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "300px",
+    },
+    child: {
+      width: "60%",
+    },
+  }));
 
+const ContactUs = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+          <div className={classes.child}>
+            <TextField
+              label="Email"
+              fullWidth
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />{" "}
+            <br />
+            <TextField
+              label="Name"
+              type="password"
+              fullWidth
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />{" "}
+            <br />
+            <br />
+            <TextField
+              label="Message"
+              type="password"
+              fullWidth
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />{" "}
+            <br />
+            <br />
+            <Button
+              variant="contained"
+              color="dark"
+              onClick={(e) => {
+                userService
+                  .login(email, password)
+                  .then((data) => {
+                    console.log(data);
+                    window.location.href = "/";
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              }}
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
+      );
+}
+ 
 export default ContactUs;
